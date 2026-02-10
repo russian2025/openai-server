f4b6a7e2465cf53bb6d58402aa148b0567126906
russian2025 <tatiana.istrati2023@gmail.com>
2026-02-10 23:30:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ function verifySignedMac(mac, timestamp, signature) {
 }
 
 app.post('/checka', (req, res) => {                  // Получение токена
-  const { aaa, t, s  } = req.body;
+  const { aaa, t, s } = req.body;
 
   if (!aaa || typeof aaa !== 'string') {
     return res.status(400).json({ error: 'Invalid request' });
@@ -201,4 +201,3 @@ app.post('/api/tts', async (req, res) => {
 app.listen(PORT, () => {                                    // Start
   console.log(`Server running on port ${PORT}`);
 });
-
